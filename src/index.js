6e9938bd20c64b50e5cda0953e82b6baae531f2f
Mateusz Garczyński <evil.khai@gmail.com>
2021-08-31 01:03:32 +0200

    ApolloClient and Provider configuration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles.css';
 import App from './App';
+
+import './styles.css';
+
+import { ApolloClient, InMemoryCache, HttpLink, gql, ApolloProvider } from "@apollo/client";
+
+const URL = "https://khayn-graphql-random.herokuapp.com/";
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({uri: URL})
+});
+
+const RANDOM_QUOTE_QUERY = gql`
+query getRandomQuote {
+  randomQuote {
+    text
+    author
+  }
+}
+`
+
+client.query({query : RANDOM_QUOTE_QUERY}).then(result => console.log("Query result: " + result.data));
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
