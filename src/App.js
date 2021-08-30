b4390a2bacdfb514e4a6f1f07e2a324dbd7f05ff
Mateusz Garczyński <evil.khai@gmail.com>
2021-08-31 01:19:53 +0200

    integration with graphql server

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import React from "react";
 import "./styles.css";
 
+import { gql, useQuery } from "@apollo/client";
+
+
+const RANDOM_QUOTE_QUERY = gql`
+query getRandomQuote {
+  randomQuote {
+    text
+    author
+  }
+}
+`
+
 export default function App() {
   return (
     <div className="App">
       <h1> Inspiring Quote </h1>
-      <Quote text="To be or not to be..." author = "Hamlet" />
+     <RandomQuote/>
     </div>
   );
+}
+
+function RandomQuote() {
+  const {data, loading} = useQuery(RANDOM_QUOTE_QUERY);
+
+  if (loading) {
+    return "Quote is loading...";
+  }
+
+  const {text, author} = data.randomQuote;
+  
+  return <Quote text={text} author={author} />;
 }
 
 function Quote({text, author}) {
--- src/index.js
+++ src/index.js
@@ -4,24 +4,14 @@ import App from './App';
 
 import './styles.css';
 
-import { ApolloClient, InMemoryCache, HttpLink, gql, ApolloProvider } from "@apollo/client";
+import { ApolloClient, InMemoryCache, HttpLink, ApolloProvider } from "@apollo/client";
 
 const URL = "https://khayn-graphql-random.herokuapp.com/";
+
 const client = new ApolloClient({
   cache: new InMemoryCache(),
   link: new HttpLink({uri: URL})
 });
-
-const RANDOM_QUOTE_QUERY = gql`
-query getRandomQuote {
-  randomQuote {
-    text
-    author
-  }
-}
-`
-
-client.query({query : RANDOM_QUOTE_QUERY}).then(result => console.log("Query result: " + result.data));
 
 
 ReactDOM.render(
